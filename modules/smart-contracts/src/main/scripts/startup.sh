71eace85785896cc93912b46fcf68b51c602a257
Zach Zaiman <zzaiman@emory.edu>
2021-02-28 14:16:17 -0500

    fixed bug in startup script

--- modules/smart-contracts/src/main/scripts/startup.sh
+++ modules/smart-contracts/src/main/scripts/startup.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/sh
+#!/bin/bash
 
 echo "========Starting Network========="
 # shellcheck disable=SC2164
-cd ../../../visue-net
-./network.sh up createChannel
+
+cd ../../../viseu-net/ ; ./network.sh up createChannel
 
 echo "========Stopping Network========="
 
