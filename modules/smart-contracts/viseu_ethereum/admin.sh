a2c626012297288dde2982ceb3f9ad9f713e2d38
zmz223 <65204992+zmz223@users.noreply.github.com>
2021-04-01 10:56:14 -0400

    Update admin.sh

--- modules/smart-contracts/viseu_ethereum/admin.sh
+++ modules/smart-contracts/viseu_ethereum/admin.sh
@@ -1,8 +1,4 @@
-echo "what node do you want to start? "
+echo "starting admin node..."
 
-read node
-
-
-
-geth --identity $node --rpc --rpcport "8000" --rpccorsdomain "*" --datadir $node --port "30303" --nodiscover --rpcapi "db,eth,net,web3,personal,miner,admin" --networkid 1900 --nat "any"
+geth --identity admin --rpc --rpcport "8000" --rpccorsdomain "*" --datadir admin/ --port "30303" --nodiscover --rpcapi "db,eth,net,web3,personal,miner,admin" --networkid 1900 --nat "any"
 
