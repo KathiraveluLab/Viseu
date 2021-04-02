722b22227c0d08484ee111e4842842274285b401
Zach Zaiman <zzaiman@emory.edu>
2021-04-01 21:08:02 -0400

    fixed bugs

--- modules/smart-contracts/viseu_ethereum/start.sh
+++ modules/smart-contracts/viseu_ethereum/start.sh
@@ -3,7 +3,7 @@
 
 
 
-if [ -d "$nodenum" ]; then
+if [ -d "$1" ]; then
     rm -rf $1
 fi
 
@@ -24,4 +24,4 @@ fi
 fuser -k 30303/tcp
 fuser -k 30323/tcp
 
-nohup eth --identity $1 --rpc --rpcport "8000" --rpccorsdomain "*" --datadir $1 --port "30303" --nodiscover --rpcapi "db,eth,net,web3,personal,miner,admin" --networkid 1900 --nat "any" > node_log.out &
+nohup geth --identity $1 --rpc --rpcport "8000" --rpccorsdomain "*" --datadir $1 --port "30303" --nodiscover --rpcapi "db,eth,net,web3,personal,miner,admin" --networkid 1900 --nat "any" > node_log.out &
