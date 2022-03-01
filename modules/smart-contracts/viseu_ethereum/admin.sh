12da7984b710bbd2945c8e6cda24086bafd575c9
pradeeban <kk.pradeeban@gmail.com>
2022-03-01 00:58:24 +0000

    refactored code

--- modules/smart-contracts/viseu_ethereum/admin.sh
+++
@@ -1,4 +0,0 @@
-echo "starting admin node..."
-
-geth --identity admin --rpc --rpcport "8000" --rpccorsdomain "*" --datadir admin/ --port "30303" --nodiscover --rpcapi "db,eth,net,web3,personal,miner,admin" --networkid 1900 --nat "any"
-
